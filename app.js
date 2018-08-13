d76b0660f01f9a217902e1611f6d23c09e2dbfb2
uriportnoy <uriportnoy@gmail.com>
2018-08-13 22:15:35 +0300

    last update of file 2

--- app.js
+++ app.js
@@ -2,19 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const io = require('socket.io-client');
-const socket = io('http://192.168.42.10:3001',{  //52.38.156.227:3001
+const socket = io('http://52.38.156.227:3001',{  //http://52.38.156.227:3001
     path:'/wiseCarePlatform',
     transports:['websocket']
 });
-
-const webServiceHandler = require('./src/webServiceHandler/webServiceHandler')(socket);
-
+require('./src/routes/registrationRoutes/registrationRoutes')(app);
+require('./src/webServiceHandler/webServiceHandler')(socket);
 
 app.get('/health', (req, res) =>
 {
     res.send('hello world!!');
 });
-
 
 app.listen(`${port}`, () => console.log(`listening on port ${port}`));
 
